d6a639af9f16170eee6129e4945ec0d94e72a90f
Julián <juliancamposperez@gmail.com>
2012-12-21 16:20:17 +0100

    Update Examples/dcMotorAdelanteAtras/dcMotorAdelanteAtras.ino
    Modificamos nombre de variables, propuestas por fuseprods

--- Examples/dcMotorAdelanteAtras/dcMotorAdelanteAtras.ino
+++ Examples/dcMotorAdelanteAtras/dcMotorAdelanteAtras.ino
@@ -1,9 +1,14 @@
 #include <DcMotor.h>
 
-DcMotor motor1(3, 8, 9); // creamos el objeto motor1. El pin 3 es el pin PWM y debe estar conectado al pin enable, el pin 8 es un pin conectado al motor y el pin 9 es otro conectado al motor
+int PinEnableMotor1 = 3;  //Pin utilizado para activar o desactivar el motor. Debe conectarse a una salida PWM.
+int Pin1Motor1 = 8;  //Pin conectado a una de las patillas del motor 1 en el puente H.
+int Pin2Motor1 = 9;  //Pin conectado a la segunda patilla del motor 1 en el puente H.
+
+
+DcMotor motor1(PinEnableMotor1, Pin1Motor1, Pin2Motor1);  //Creamos el objeto motor1. 
                          
-  int velocidadAvance = 200; //definimos variables con las velocidades
-  int velocidadAtras = 80; //la velocidad la tenemos que definir en el rango del PWM, es decir, entre 0 y 255, siendo 0 -> 0V y 255 -> 5V continuos.       
+int velocidadAvance = 200; //definimos variables con las velocidades
+int velocidadAtras = 80; //la velocidad la tenemos que definir en el rango del PWM, es decir, entre 0 y 255, siendo 0 -> 0V y 255 -> 5V continuos.       
 
 void setup () {
                           
@@ -11,12 +16,12 @@ void setup () {
 }
 
 void loop(){
-   motor1.avanzar(velocidadAvance);   // llamamos a la funcion avanzar a una velocidad de 200
+
+   motor1.avance(velocidadAvance);   // llamamos a la funcion avanzar a una velocidad de 200
    delay(5000);           //esperamos 5 segundos mientras se mueve el motor
-   motor1.atras(velocidadAtras);      //llamamos a la funcion atras a una velocidad de 80
+   motor1.retroceso(velocidadAtras);      //llamamos a la funcion atras a una velocidad de 80
    delay(5000);           //esperamos 5 segundos mientras se mueve hacia atras
    motor1.parar();        //llamamos a la funcion parar para detener el motor
    delay(3000);           //lo detenemos durante 3 segundos
-   
 
 }

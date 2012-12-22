4099a1b635cec27875d0847032c3aa705eeddf60
Julián <juliancamposperez@gmail.com>
2012-12-22 18:08:44 +0100

    Update Examples/dcMotorAdelanteAtras/dcMotorAdelanteAtras.ino

--- Examples/dcMotorAdelanteAtras/dcMotorAdelanteAtras.ino
+++ Examples/dcMotorAdelanteAtras/dcMotorAdelanteAtras.ino
@@ -6,16 +6,17 @@ int Pin2Motor1 = 9;  //Pin conectado a la segunda patilla del motor 1 en el puen
 
 
 DcMotor motor1(PinEnableMotor1, Pin1Motor1, Pin2Motor1);  //Creamos el objeto motor1. 
+       
                          
 void setup () {
-                          
-int velocidadAvance = 200; //definimos variables con las velocidades
-int velocidadAtras = 80; //la velocidad la tenemos que definir en el rango del PWM, es decir, entre 0 y 255, siendo 0 -> 0V y 255 -> 5V continuos.       
+                               
 
 }
 
 void loop(){
 
+   int velocidadAvance = 200; //definimos variables con las velocidades
+   int velocidadAtras = 80; //la velocidad la tenemos que definir en el rango del PWM, es decir, entre 0 y 255, siendo 0 -> 0V y 255 -> 5V continuos.
    motor1.avance(velocidadAvance);   // llamamos a la funcion avanzar a una velocidad de 200
    delay(5000);           //esperamos 5 segundos mientras se mueve el motor
    motor1.retroceso(velocidadAtras);      //llamamos a la funcion atras a una velocidad de 80
